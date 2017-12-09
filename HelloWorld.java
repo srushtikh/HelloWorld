79809d03ca369a9d513ab884244ae60a0035eb23
srushti <srushti@test.com>
2017-12-09 12:12:02 +0530

    Modified code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,3 @@
-/*public class HelloWorld{
-	public static void main(String[] args){
-		System.Out.println("Hello World!!");
-	}
-}*/
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );

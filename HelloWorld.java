504b5256083de833404435915d26540b4ce2a9ea
srushti <srushti@test.com>
2017-12-09 12:09:02 +0530

    Updated code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
-public class HelloWorld{
+/*public class HelloWorld{
 	public static void main(String[] args){
 		System.Out.println("Hello World!!");
 	}
+}*/
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
 }
